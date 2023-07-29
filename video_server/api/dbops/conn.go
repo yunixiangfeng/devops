a9c183b47f68ad1f059913c348bedd0339e7ccc8
wu123 <535504958@qq.com>
2023-07-29 23:07:45 +0800

    create video_server

---
+++ video_server/api/dbops/conn.go
@@ -0,0 +1,22 @@
+package dbops
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	dbConn *sql.DB
+	err    error
+)
+
+func init() {
+	fmt.Println("Entering conn.go init function...")
+	dbConn, err = sql.Open("mysql", "root:1234@/video_server?charset=utf8")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("dbConn +%v\n", dbConn)
+}
---
+++ video_server/go.mod
@@ -0,0 +1,8 @@
+module video_server
+
+go 1.18
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/julienschmidt/httprouter v1.3.0
+)
