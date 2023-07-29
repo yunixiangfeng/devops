85777f72e8868a33c88fd498b58666b18b063110
wu123 <535504958@qq.com>
2023-07-29 11:09:39 +0800

    create router

---
+++ gin_restful/router/router.go
@@ -0,0 +1,27 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/", IndexUsers)
+
+	// 路由群组
+	users := router.Group("api/v1/users")
+	{
+		users.GET("", GetAll)
+		users.POST("/add", AddUsers)
+		users.GET("/get/:id", GetOne)
+		users.POST("/update", UpdateUser)
+		users.POST("/del", DelUser)
+	}
+	departments := router.Group("api/v1/department")
+	{
+		departments.GET("", GetAll)
+		departments.POST("/add", AddUsers)
+		departments.GET("/get/:id", GetOne)
+		departments.POST("/update", UpdateUser)
+		departments.POST("/del", DelUser)
+	}
+	return router
+}
